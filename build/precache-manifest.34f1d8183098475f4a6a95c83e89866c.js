ec07f25a741da5dbe6f4cf436cfceb04706c3e1e
Your Name <rohan@example.com>
2022-10-04 15:07:28 -0400

    React project code added

---
+++ build/precache-manifest.34f1d8183098475f4a6a95c83e89866c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9649fe220c3f9d557685",
+    "url": "/vraj38/comp3104_lab_f2022/static/css/main.9994040a.chunk.css"
+  },
+  {
+    "revision": "9649fe220c3f9d557685",
+    "url": "/vraj38/comp3104_lab_f2022/static/js/main.9649fe22.chunk.js"
+  },
+  {
+    "revision": "6a724995c0aa4395f82f",
+    "url": "/vraj38/comp3104_lab_f2022/static/js/runtime~main.6a724995.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/vraj38/comp3104_lab_f2022/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "6c4e34f0890680301967883c012f7764",
+    "url": "/vraj38/comp3104_lab_f2022/index.html"
+  }
+];
